ee78c3f6c68ededd21a74600c5624a3044a96d68
ANINUL ISLAM EFAT <aminulifu01@gmail.com>
2026-07-29 00:59:19 +0600

    Create InputOutput.c
    Implement basic input and output for various data types.

---
+++ Basic/InputOutput.c
@@ -0,0 +1,37 @@
+/**
+ * C programming basic input output
+ */
+ #include <stdio.h>
+ int main(){
+     /**
+      * All primitive and derived types
+      */
+      char charvalue;
+      unsigned char uchar;
+      short shortvalue;
+      unsigned short ushort;
+      int intvalue;
+      unsigned uint;
+      long longvalue;
+      unsigned long ulong;
+      long long longlongvalue;
+      unsigned long long ulonglong;
+      float floatvalue;
+      double doublevalue;
+      long double longdouble;
+      /**
+       *Input secstion
+       */
+       printf("Enter a character =");
+       charvalue = getchar();    // getchar() function to input a character
+       printf("Enter one another character = ");
+       uchar = getchar();
+       
+       printf("Enter an signed shortvalue = ");
+       scanf("%hi", &shortvalue);
+       
+       printf("")
+       
+       
+      
+ }
